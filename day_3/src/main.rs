89585b85bec793e53905e075e481412bd71efb3c
Jordan Delbar <jordandelbar@gmail.com>
2024-12-03 13:34:25 +0100

    Add day 3 part 1

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,12 +1,19 @@
+use utils::read_to_string;
 use regex::Regex;
 
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("../data/input_d3.txt").unwrap();
+    let result = find_pattern(&input);
+    println!("{}", result);
 }
 
-fn find_pattern(input: &str) -> usize {
-    let re = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").expect("Failed to compile regex");
-    re.find_iter(input).count()
+fn find_pattern(input: &str) -> i32 {
+    let mut result = 0;
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("Failed to compile regex");
+    for capture in re.captures_iter(input) {
+        result += capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap()
+    }
+    result
 }
 
 #[cfg(test)]
@@ -15,10 +22,10 @@ mod tests {
 
     #[test]
     fn test_find_pattern() {
-        let input = String::from("wermul(3,2)warawpermul(2,4)");
+        let input = String::from("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
 
         let got = find_pattern(&input);
-        let want: usize = 2;
+        let want = 161;
         assert_eq!(got, want);
     }
 }
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,9 +1,16 @@
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Read};
 
 fn read_file(file_path: &str) -> io::BufReader<File> {
     let file = File::open(file_path).expect("File not found");
     io::BufReader::new(file)
+}
+
+pub fn read_to_string(file_path: &str) -> io::Result<String> {
+    let mut buf_reader = read_file(file_path);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    Ok(contents)
 }
 
 pub fn read_input_d1(file_path: &str) -> (Vec<u32>, Vec<u32>) {
