f9c5db1f0d237bb972155ed854328fac7778aa59
Jordan Delbar <jordandelbar@gmail.com>
2024-12-03 22:31:21 +0100

    Improve day 3

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -2,12 +2,24 @@ use utils::read_to_string;
 use regex::Regex;
 
 fn main() {
-    let input = read_to_string("../data/input_d3.txt").unwrap();
-    let result = find_pattern(&input);
-    println!("{}", result);
+    let input = read_to_string("../data/input_d3.txt").expect("Failed to read input");
+    let result_part1 = find_pattern(&input);
+    let result_part2 = find_pattern_with_activation(&input);
+    println!("result for part 1: {}", result_part1);
+    println!("result for part 2: {}", result_part2);
 }
 
 fn find_pattern(input: &str) -> i32 {
+    let mut result = 0;
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("Failed to compile regex");
+    for capture in re.captures_iter(input) {
+        result += capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap()
+    }
+
+    result
+}
+
+fn find_pattern_with_activation(input: &str) -> i32 {
     let mut result = 0;
     let mut is_active = true;
     let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|(don\'t)|(do)").expect("Failed to compile regex");
@@ -33,19 +45,36 @@ fn find_pattern(input: &str) -> i32 {
 mod tests {
     use super::*;
 
+    struct TestCase {
+        input: String,
+        expected: i32,
+    }
+
     #[test]
     fn test_find_pattern() {
-        let input = String::from("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+        let test_cases = vec![
+            // Example data
+            TestCase {
+                input: String::from("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
+                expected: 161,
+            },
+            // Too long decimal
+            TestCase {
+                input: String::from("xmul(2234,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
+                expected: 153,
+            },
+        ];
 
-        let got = find_pattern(&input);
-        let want = 161;
-        assert_eq!(got, want);
+        for case in test_cases {
+            let got = find_pattern(&case.input);
+            assert_eq!(got, case.expected, "Failed for input: {:?}", case.input)
+        }
     }
 
     #[test]
     fn test_find_pattern_with_activation() {
         let input = String::from("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
-        let got = find_pattern(&input);
+        let got = find_pattern_with_activation(&input);
         let want = 48;
         assert_eq!(got, want);
     }
