e3ac927893fafea7f06b38f234ae671a9ee9956a
Jordan Delbar <jordandelbar@gmail.com>
2024-12-03 13:58:50 +0100

    Add day 3 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -9,9 +9,22 @@ fn main() {
 
 fn find_pattern(input: &str) -> i32 {
     let mut result = 0;
-    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("Failed to compile regex");
+    let mut active = 1;
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|(don\'t)|(do)").expect("Failed to compile regex");
     for capture in re.captures_iter(input) {
-        result += capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap()
+        let capture_1 = capture.get(1).map_or(0, |m| m.as_str().parse::<i32>().unwrap());
+        let capture_2 = capture.get(2).map_or(0, |m| m.as_str().parse::<i32>().unwrap());
+        let dont_value = capture.get(3).map_or("", |m| m.as_str());
+        let do_value = capture.get(4).map_or("", |m| m.as_str());
+        if !do_value.is_empty() && do_value == "do" {
+            active = 1;
+        }
+        if !dont_value.is_empty() && dont_value == "don't" {
+            active = 0;
+        }
+        if active == 1 {
+            result += capture_1 * capture_2;
+        }
     }
     result
 }
@@ -26,6 +39,14 @@ mod tests {
 
         let got = find_pattern(&input);
         let want = 161;
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn test_find_pattern_with_activation() {
+        let input = String::from("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+        let got = find_pattern(&input);
+        let want = 48;
         assert_eq!(got, want);
     }
 }
