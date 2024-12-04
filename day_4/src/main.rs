fa2cc978d48fcba11ee8d0819f0510bcd528937f
Jordan Delbar <jordandelbar@gmail.com>
2024-12-04 13:26:36 +0100

    Refactor day 4

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -2,66 +2,65 @@ use utils::read_input_d4;
 
 fn main() {
     let input = read_input_d4("../data/input_d4.txt");
-    let v = look_for_xmas(&input);
-    let mut count = 0;
-    for catch in v {
-        if is_xmas(&catch) {
-            count +=1;
-        }
-    }
-    println!("{}", count)
+    let sequences = look_for_xmas(&input);
+    let first_part_count = sequences.iter().filter(|seq| is_xmas(seq)).count();
+    println!("{}", first_part_count);
 }
 
-// look_for_xmas returns a vector of maximum 8 strings that contains
-// the next 3 characters in every direction (horizontal, vertical, diagonal and left and right)
+// look_for_xmas returns a vector of maximum 4 strings that contains
+// the next 3 characters in every direction (horizontal, vertical, diagonal)
 // if the index goes out of bound the record is discarded
-fn look_for_xmas(input: &Vec<Vec<char>>) -> Vec<String> {
-    let mut sequence = Vec::new();
+fn look_for_xmas(input: &[Vec<char>]) -> Vec<String> {
+    let mut sequences = Vec::new();
 
-    for row_index in 0..input.len() {
-        for col_index in 0..input[row_index].len() {
-            if input[row_index][col_index] == 'X' || input[row_index][col_index] == 'S' {
-                // horizontal
-                if col_index < input[row_index].len() - 3 {
-                    let mut string_to_check = String::new();
-                    string_to_check.push(input[row_index][col_index]);
-                    string_to_check.push(input[row_index][col_index + 1]);
-                    string_to_check.push(input[row_index][col_index + 2]);
-                    string_to_check.push(input[row_index][col_index + 3]);
-                    sequence.push(string_to_check);
-                }
-                // diagonal east
-                if row_index < input.len() - 3 && col_index < input[row_index].len() - 3 {
-                    let mut string_to_check = String::new();
-                    string_to_check.push(input[row_index][col_index]);
-                    string_to_check.push(input[row_index + 1][col_index + 1]);
-                    string_to_check.push(input[row_index + 2][col_index + 2]);
-                    string_to_check.push(input[row_index + 3][col_index + 3]);
-                    sequence.push(string_to_check);
-                }
-                // diagonal west
-                if row_index < input.len() - 3 && col_index >= 3 {
-                    let mut string_to_check = String::new();
-                    string_to_check.push(input[row_index][col_index]);
-                    string_to_check.push(input[row_index + 1][col_index - 1]);
-                    string_to_check.push(input[row_index + 2][col_index - 2]);
-                    string_to_check.push(input[row_index + 3][col_index - 3]);
-                    sequence.push(string_to_check);
-                }
-                // vertical
-                if row_index < input.len() - 3 {
-                    let mut string_to_check = String::new();
-                    string_to_check.push(input[row_index][col_index]);
-                    string_to_check.push(input[row_index + 1][col_index]);
-                    string_to_check.push(input[row_index + 2][col_index]);
-                    string_to_check.push(input[row_index + 3][col_index]);
-                    sequence.push(string_to_check);
-                }
+    for (row_index, row) in input.iter().enumerate() {
+        for col_index in 0..row.len() {
+            if matches!(input[row_index][col_index], 'X' | 'S') {
+                sequences.extend(
+                    [
+                        (1, 0),  // Vertical
+                        (0, 1),  // Horizontal
+                        (1, 1),  // Diagonal east
+                        (1, -1), // Diagonal west
+                    ]
+                    .iter()
+                    .filter_map(|&(row_step, col_step)| {
+                        extract_sequence(input, row_index, col_index, row_step, col_step)
+                    }),
+                );
             }
         }
     }
 
-    sequence
+    sequences
+}
+
+fn extract_sequence(
+    input: &[Vec<char>],
+    start_row: usize,
+    start_col: usize,
+    row_step: isize,
+    col_step: isize,
+) -> Option<String> {
+    let mut sequence = String::new();
+    let mut row = start_row as isize;
+    let mut col = start_col as isize;
+
+    for _ in 0..4 {
+        // Check if out of bound
+        if row < 0
+            || col < 0
+            || row >= input.len() as isize
+            || col >= input[row as usize].len() as isize
+        {
+            return None;
+        }
+        sequence.push(input[row as usize][col as usize]);
+        row += row_step;
+        col += col_step;
+    }
+
+    Some(sequence)
 }
 
 fn is_xmas(word: &str) -> bool {
@@ -77,10 +76,21 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_is_xmas() {
+        assert!(is_xmas("XMAS"));
+        assert!(is_xmas("SAMX"));
+        assert!(!is_xmas("ABCD"));
+    }
+
+    #[test]
     fn test_look_for_xmas() {
-        let input = vec![vec!['X', 'M', 'A', 'S'], vec!['X', 'M', 'B', 'D'], vec!['S', 'A', 'M', 'X']];
+        let input = vec![
+            vec!['X', 'M', 'A', 'S'],
+            vec!['X', 'M', 'B', 'D'],
+            vec!['S', 'A', 'M', 'X'],
+        ];
         let got = look_for_xmas(&input);
-        let want = vec!["XMAS".to_string(), "XMBD".to_string(), "SAMX".to_string()];
+        let want = vec!["XMAS", "XMBD", "SAMX"];
         assert_eq!(want, got);
     }
 }
