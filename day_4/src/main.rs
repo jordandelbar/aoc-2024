e777fcbc9fdc6be4cccb98d5e9d3e785bf8fc469
Jordan Delbar <jordandelbar@gmail.com>
2024-12-04 09:02:31 +0100

    Refactor horizontal

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -2,11 +2,33 @@ use utils::*;
 
 fn main() {
     let input = read_input_d4("../data/input_d4.txt");
-    println!("Part 1: {:?}", &input);
+    catch_xmas(&input);
 }
 
-fn catch_xmas(_input: &Vec<Vec<char>>) -> i32 {
-    return 1
+fn catch_xmas(input: &Vec<Vec<char>>) -> i32 {
+    let mut count = 0;
+    for row in input {
+        for index in 0..row.len() - 3 {
+            let mut string_to_check = String::new();
+            string_to_check.push(row[index]);
+            string_to_check.push(row[index + 1]);
+            string_to_check.push(row[index + 2]);
+            string_to_check.push(row[index + 3]);
+
+            if is_xmas(&string_to_check) {
+                count += 1;
+            }
+        }
+    }
+
+    count
+}
+
+fn is_xmas(word: &str) -> bool {
+    match word {
+        "XMAS" => true,
+        _ => false,
+    }
 }
 
 #[cfg(test)]
