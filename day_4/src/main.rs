6395332864e8798fec072f5bab977dcbdc724d44
Jordan Delbar <jordandelbar@gmail.com>
2024-12-04 18:10:25 +0100

    Day 4 second part

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -2,9 +2,11 @@ mod first_part;
 mod second_part;
 
 use first_part::*;
+use second_part::*;
 use utils::read_input_d4;
 
 fn main() {
     let input = read_input_d4("../data/input_d4.txt");
     println!("first part result: {}", count_number_xmas(&input));
+    println!("second part result: {}", look_for_xmas_cross(&input));
 }
--- day_4/src/second_part.rs
+++ day_4/src/second_part.rs
@@ -1,1 +1,55 @@
+pub fn look_for_xmas_cross(input: &Vec<Vec<char>>) -> u32 {
+    let mut count = 0;
 
+    for row_index in 1..input.len() - 1 {
+        for col_index in 1..input[row_index].len() - 1 {
+            if input[row_index][col_index] == 'A' {
+                let diag_north_west = input[row_index - 1][col_index - 1];
+                let diag_south_east = input[row_index + 1][col_index + 1];
+                let diag_north_east = input[row_index - 1][col_index + 1];
+                let diag_south_west = input[row_index + 1][col_index - 1];
+
+                let diag_1 = format!("{}A{}", diag_north_west, diag_south_east);
+                let diag_2 = format!("{}A{}", diag_north_east, diag_south_west);
+
+                if check_mas(&diag_1, &diag_2) {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    count
+}
+
+fn check_mas(diag_1: &str, diag_2: &str) -> bool {
+    if matches!(diag_1, "MAS" | "SAM") && matches!(diag_2, "MAS" | "SAM") {
+        true
+    } else {
+        false
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_thing() {
+        let matrix: Vec<Vec<char>> = vec![
+            vec!['M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'],
+            vec!['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'],
+            vec!['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'],
+            vec!['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'],
+            vec!['X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'],
+            vec!['X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'],
+            vec!['S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'],
+            vec!['S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'],
+            vec!['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'],
+            vec!['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X'],
+        ];
+
+        let got = look_for_xmas_cross(matrix);
+        let want = 9;
+        assert_eq!(got, want)
+    }
+}
