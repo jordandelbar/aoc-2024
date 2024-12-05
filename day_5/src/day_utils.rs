9dbe6ab24728023d5a71911cf3a283a03e54001e
Jordan Delbar <jordandelbar@gmail.com>
2024-12-05 18:37:11 +0100

    Add day 5 reading

---
+++ day_5/src/day_utils.rs
@@ -0,0 +1,31 @@
+use std::io::BufRead;
+use utils::read_file;
+
+pub fn read_input(file_path: &str) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
+    let input = read_file(file_path);
+    let mut lines = input.lines();
+
+    let mut pairs = Vec::new();
+    while let Some(Ok(line)) = lines.next() {
+        if line.contains('|') {
+            let nums: Vec<i32> = line.split('|')
+                .map(|n| n.parse().unwrap())
+                .collect();
+            pairs.push((nums[0], nums[1]));
+        } else if line.is_empty() {
+            break;
+        }
+    }
+
+    let mut vectors = Vec::new();
+    for line in lines {
+        if let Ok(line) = line {
+            let vec_nums: Vec<i32> = line.split(',')
+                .map(|n| n.parse().unwrap())
+                .collect();
+            vectors.push(vec_nums);
+        }
+    }
+
+    (pairs, vectors)
+}
--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,3 +1,6 @@
+mod day_utils;
+
 fn main() {
-    println!("Hello, world!");
+    let (test1, test2) = day_utils::read_input("../data/input_d5.txt");
+    println!("{:?}, {:?}", test1, test2);
 }
