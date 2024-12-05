e1e4603832ad1ac5adbf22c8cb487c78978c6789
Jordan Delbar <jordandelbar@gmail.com>
2024-12-05 19:19:56 +0100

    Add day 5 dict rule

--- day_5/src/day_utils.rs
+++ day_5/src/day_utils.rs
@@ -8,9 +8,7 @@ pub fn read_input(file_path: &str) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
     let mut pairs = Vec::new();
     while let Some(Ok(line)) = lines.next() {
         if line.contains('|') {
-            let nums: Vec<i32> = line.split('|')
-                .map(|n| n.parse().unwrap())
-                .collect();
+            let nums: Vec<i32> = line.split('|').map(|n| n.parse().unwrap()).collect();
             pairs.push((nums[0], nums[1]));
         } else if line.is_empty() {
             break;
@@ -20,9 +18,7 @@ pub fn read_input(file_path: &str) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
     let mut vectors = Vec::new();
     for line in lines {
         if let Ok(line) = line {
-            let vec_nums: Vec<i32> = line.split(',')
-                .map(|n| n.parse().unwrap())
-                .collect();
+            let vec_nums: Vec<i32> = line.split(',').map(|n| n.parse().unwrap()).collect();
             vectors.push(vec_nums);
         }
     }
--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,6 +1,53 @@
 mod day_utils;
+use std::collections::HashMap;
 
 fn main() {
-    let (test1, test2) = day_utils::read_input("../data/input_d5.txt");
-    println!("{:?}, {:?}", test1, test2);
+    let (rules, pages) = day_utils::read_input("../data/input_d5.txt");
+    let my_dict = create_rule_dict(rules);
+    let following_pages = get_following_pages(0, &pages[0]);
+    println!("{:?}", following_pages);
+}
+
+fn create_rule_dict(pairs: Vec<(i32, i32)>) -> HashMap<i32, Vec<i32>> {
+    let mut dict: HashMap<i32, Vec<i32>> = HashMap::new();
+
+    for (key, value) in pairs {
+        dict.entry(key).or_insert_with(Vec::new).push(value);
+    }
+
+    dict
+}
+
+fn get_following_pages(index: u32, input: &Vec<i32>) -> Vec<i32> {
+    let mut output = Vec::new();
+    let index = index + 1;
+    for i in index as usize..input.len() {
+        output.push(input[i]);
+    }
+
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_rule_dictionary() {
+        let input = vec![(87, 5), (54, 3), (54, 4), (87, 2)];
+        let want: HashMap<i32, Vec<i32>> = [(87, vec![5, 2]), (54, vec![3, 4])]
+            .iter()
+            .cloned()
+            .collect();
+        let got = create_rule_dict(input);
+        assert_eq!(want, got);
+    }
+
+    #[test]
+    fn test_get_following_pages() {
+        let input = vec![5, 4, 3, 2, 1];
+        let want = vec![3, 2, 1];
+        let got = get_following_pages(1, &input);
+        assert_eq!(want, got);
+    }
 }
