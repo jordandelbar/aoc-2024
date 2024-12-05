f299bf1bceab460ceae7a7d8a54d8b98af9e32b3
Jordan Delbar <jordandelbar@gmail.com>
2024-12-05 22:08:25 +0100

    Day 4 wip

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -2,13 +2,38 @@ mod day_utils;
 use std::collections::HashMap;
 
 fn main() {
-    let (rules, pages) = day_utils::read_input("../data/input_d5.txt");
-    let my_dict = create_rule_dict(rules);
-    let following_pages = get_following_pages(0, &pages[0]);
-    println!("{:?}", following_pages);
+    let (rules, updates) = day_utils::read_input("../data/input_d5.txt");
+    let rules_dict = create_rules_dict(rules);
+    let count = compute_middle_page_number(rules_dict, updates);
+    println!("first part result: {}", count);
 }
 
-fn create_rule_dict(pairs: Vec<(i32, i32)>) -> HashMap<i32, Vec<i32>> {
+fn compute_middle_page_number(rules_dict: HashMap<i32, Vec<i32>>, updates: Vec<Vec<i32>>) -> i32 {
+    let mut count = 0;
+    for update in updates {
+        println!("update: {:?}", &update);
+        for (idx, &page_number) in update.iter().enumerate() {
+            let preceding_pages = get_preceding_pages(idx, &update);
+            println!("preceding_pages: {:?}", &preceding_pages);
+
+            if let Some(rules_values) = rules_dict.get(&page_number) {
+                if preceding_pages.iter().any(|x| rules_values.contains(x)) {
+                    println!("has been triggered because: {:?}", rules_values);
+                    count += find_middle_number(&update);
+                    break
+                }
+            }
+        }
+    }
+
+    count
+}
+
+fn find_middle_number(vector: &Vec<i32>) -> i32 {
+    vector[vector.len() / 2]
+}
+
+fn create_rules_dict(pairs: Vec<(i32, i32)>) -> HashMap<i32, Vec<i32>> {
     let mut dict: HashMap<i32, Vec<i32>> = HashMap::new();
 
     for (key, value) in pairs {
@@ -18,14 +43,14 @@ fn create_rule_dict(pairs: Vec<(i32, i32)>) -> HashMap<i32, Vec<i32>> {
     dict
 }
 
-fn get_following_pages(index: u32, input: &Vec<i32>) -> Vec<i32> {
-    let mut output = Vec::new();
-    let index = index + 1;
-    for i in index as usize..input.len() {
-        output.push(input[i]);
-    }
+fn get_preceding_pages(index: usize, input: &[i32]) -> Vec<i32> {
+    input[..index].iter().rev().cloned().collect()
+}
 
-    output
+fn generate_preceding_pages(input: &[i32]) -> Vec<Vec<i32>> {
+    (0..input.len())
+        .map(|index| get_preceding_pages(index, input))
+        .collect::<Vec<Vec<i32>>>()
 }
 
 #[cfg(test)]
@@ -44,10 +69,19 @@ mod tests {
     }
 
     #[test]
-    fn test_get_following_pages() {
-        let input = vec![5, 4, 3, 2, 1];
-        let want = vec![3, 2, 1];
-        let got = get_following_pages(1, &input);
-        assert_eq!(want, got);
+    fn test_get_preceding_pages() {
+        let pages = vec![1, 2, 3, 4, 5];
+        let result = get_preceding_pages(3, &pages);
+        assert_eq!(result, vec![3, 2, 1]);
+    }
+
+    #[test]
+    fn test_generate_preceding_pages() {
+        let pages = vec![1, 2, 3, 4, 5];
+        let result = generate_preceding_pages(&pages);
+        assert_eq!(
+            result,
+            vec![vec![], vec![1], vec![2, 1], vec![3, 2, 1], vec![4, 3, 2, 1]]
+        );
     }
 }
