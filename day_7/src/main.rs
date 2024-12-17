f873fe4678fd6e540a2cb0b24a5286e5f8feeea1
Jordan Delbar <jordandelbar@gmail.com>
2024-12-17 10:09:49 +0100

    Day 7 part 2

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,68 +1,90 @@
 mod day_utils;
 
+enum Operations {
+    Addition,
+    Multiplication,
+    Concatenation,
+}
+
+#[derive(Debug)]
+struct Line {
+    target: u64,
+    parts: Vec<u64>,
+}
+
 fn main() {
     let file_path = "../data/input_d7.txt";
-    let input = day_utils::parse_input(file_path).unwrap();
-    let mut total_calibration_result = 0;
+    let input_data = utils::read_to_string(file_path).expect("Failed to read input");
 
-    for (target, numbers) in &input {
-        if can_match_target(*target, numbers) {
-            println!("Target {} can be matched!", target);
-            total_calibration_result += target;
-        } else {
-            println!("Target {} cannot be matched.", target);
-        }
-    }
+    let total_sum = sum_valid_targets(&input_data);
 
-    println!("Total calibration result: {}", total_calibration_result);
+    println!("Total valid target sum: {}", total_sum);
 }
 
-fn can_match_target(target: i64, numbers: &[i64]) -> bool {
-    fn backtrack(current_value: i64, index: usize, target: i64, numbers: &[i64]) -> bool {
-        if index == numbers.len() {
-            return current_value == target;
+fn sum_valid_targets(input: &str) -> u64 {
+    let lines: Vec<Line> = input.trim().lines().map(|x| -> Line {
+        let sides = x.split(":").collect::<Vec<&str>>();
+
+        Line {
+            target: sides[0].parse::<u64>().unwrap(),
+            parts: sides[1].split_whitespace().map(|x| x.parse::<u64>().unwrap()).collect()
         }
+    }).collect();
 
-        let next_num = numbers[index];
-
-        let add_result = backtrack(current_value + next_num, index + 1, target, numbers);
-        let multiply_result = backtrack(current_value * next_num, index + 1, target, numbers);
-
-        add_result || multiply_result
-    }
-
-    backtrack(numbers[0], 1, target, numbers)
+    lines
+        .into_iter()
+        .filter(is_target_reachable)
+        .fold(0, |acc, line| acc + line.target)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn apply_operation(left_number: u64, right_number: u64, operation: &Operations) -> u64 {
+    match operation {
+        Operations::Addition => left_number + right_number,
+        Operations::Multiplication => left_number * right_number,
+        Operations::Concatenation => concat(left_number, right_number),
+    }
+}
 
-    #[derive(Debug)]
-    struct TestCase {
-        target: i64,
-        input: Vec<i64>,
-        expected: bool,
+fn concat(a: u64, b: u64) -> u64 {
+    let concatenated = format!("{}{}", a, b);
+    concatenated.parse::<u64>().expect("Failed to parse concatenated to i64")
+}
+
+fn generate_operator_variants(code: u64, len: usize) -> Vec<Operations> {
+    let mut operations: Vec<Operations> = Vec::new();
+    let mut steps = code;
+
+    for _ in 0..len {
+        let remainder = steps.wrapping_rem(3);
+        match remainder {
+            0 => operations.push(Operations::Addition),
+            1 => operations.push(Operations::Multiplication),
+            2 => operations.push(Operations::Concatenation),
+            _ => unreachable!(),
+        }
+        steps = steps.checked_div(3).unwrap();
     }
 
-    #[test]
-    fn test_can_match_target() {
-        let test_cases = vec! [
-            TestCase {
-                target: 20,
-                input: vec![4, 5],
-                expected: true
-            },
-            TestCase {
-                target: 20,
-                input: vec![4, 3, 2],
-                expected: false
-            },
-        ];
+    operations
+}
 
-        for case in test_cases {
-            let got = can_match_target(case.target, &case.input);
-            assert_eq!(case.expected, got);
+fn is_target_reachable(line: &Line) -> bool {
+    let gaps = line.parts.len() - 1;
+
+    'outer: for i in 0..3u32.pow(gaps as u32) {
+        let operations = generate_operator_variants(i as u64, gaps);
+
+        let mut total = line.parts[0];
+        for (j, _) in operations.iter().enumerate().take(line.parts.len() - 1) {
+            total = apply_operation(total,line.parts[j + 1], &operations[j]);
+            if total > line.target {
+                continue 'outer;
+            }
+        }
+
+        if total == line.target {
+            return true;
         }
     }
+    false
 }
