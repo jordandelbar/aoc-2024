b5521b0ed1f3b6fab10196851dd6144ab62a5b97
Jordan Delbar <jordandelbar@gmail.com>
2024-12-23 09:52:20 +0100

    Day 8 find frequencies

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -1,21 +1,33 @@
+use std::collections::HashSet;
 use utils::{read_file_to_string, string_to_grid};
 
 fn main() {
     let input = read_file_to_string("../data/input_d8.txt").unwrap();
     let test: Vec<Vec<char>> = string_to_grid(&input, false);
-    println!("Test part 1: {:?}", test);
+    let result = find_all_frequencies(test);
+    println!("{:?}", result);
+}
+
+fn find_all_frequencies(grid: Vec<Vec<char>>) -> HashSet<char> {
+    grid.iter()
+        .flat_map(|line| line.iter())
+        .filter(|&&c| c != '.')
+        .copied()
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashSet;
 
     #[test]
-    fn test_string_to_grid() {
-        let input = ".....\n.....";
+    fn test_find_all_frequencies() {
+        let input = "..3..\n..A..";
 
-        let got: Vec<Vec<char>> = string_to_grid(&input, false);
-        let want = vec![vec!['.', '.', '.', '.', '.'],vec!['.', '.', '.', '.', '.']];
+        let grid: Vec<Vec<char>> = string_to_grid(&input, false);
+        let got = find_all_frequencies(grid);
+        let want= HashSet::from(['3', 'A']);
 
         assert_eq!(want, got);
     }
